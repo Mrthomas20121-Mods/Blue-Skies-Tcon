81b694c3a82f028635c32b1644b6080d21b639f4
paypur <pieceofpaypur@gmail.com>
2025-05-22 12:32:41 -0700

    Changed material stats
    Took 1 hour 41 minutes

--- src/main/java/mrthomas20121/blue_skies_tcon/datagen/BlueMaterials.java
+++ src/main/java/mrthomas20121/blue_skies_tcon/datagen/BlueMaterials.java
@@ -3,6 +3,7 @@ package mrthomas20121.blue_skies_tcon.datagen;
 import mrthomas20121.blue_skies_tcon.BlueSkiesTcon;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Tiers;
 import slimeknights.tconstruct.library.data.material.AbstractMaterialDataProvider;
 import slimeknights.tconstruct.library.data.material.AbstractMaterialStatsDataProvider;
 import slimeknights.tconstruct.library.data.material.AbstractMaterialTraitDataProvider;
@@ -14,13 +15,11 @@ import slimeknights.tconstruct.tools.stats.StatlessMaterialStats;
 
 import javax.annotation.Nonnull;
 
-import static net.minecraft.world.item.Tiers.DIAMOND;
-
 public class BlueMaterials extends AbstractMaterialDataProvider {
 
     public static final MaterialId aquite = createMaterial("aquite");
-    public static final MaterialId diopside = createMaterial("diopside");
     public static final MaterialId charoite = createMaterial("charoite");
+    public static final MaterialId diopside = createMaterial("diopside");
     public static final MaterialId horizonite = createMaterial("horizonite");
     public static final MaterialId pyrope = createMaterial("pyrope");
 
@@ -34,11 +33,11 @@ public class BlueMaterials extends AbstractMaterialDataProvider {
 
     @Override
     protected void addMaterials() {
-        addMaterial(aquite, 3, ORDER_HARVEST, true);
-        addMaterial(charoite, 3, ORDER_WEAPON, true);
-        addMaterial(diopside, 3, ORDER_WEAPON, true);
-        addMaterial(horizonite, 3, ORDER_WEAPON, false);
-        addMaterial(pyrope, 3, ORDER_GENERAL, true);
+        addMaterial(aquite, 2, ORDER_HARVEST, true);
+        addMaterial(charoite, 2, ORDER_WEAPON, true);
+        addMaterial(diopside, 2, ORDER_WEAPON, true);
+        addMaterial(horizonite, 2, ORDER_HARVEST, false);
+        addMaterial(pyrope, 2, ORDER_HARVEST, true);
     }
 
     @Nonnull
@@ -48,18 +47,18 @@ public class BlueMaterials extends AbstractMaterialDataProvider {
     }
 
     public static class BlueSkiesTraits extends AbstractMaterialTraitDataProvider {
-
         public BlueSkiesTraits(PackOutput packOutput, AbstractMaterialDataProvider materials) {
             super(packOutput, materials);
         }
 
         @Override
         protected void addMaterialTraits() {
-            addDefaultTraits(aquite, TinkerModifiers.overworked); // TODO: no default
-            addDefaultTraits(charoite, TinkerModifiers.piercing); // TODO: not what it was
-            addDefaultTraits(diopside, TinkerModifiers.golden);
+            // TODO: everything needs to be changed
+            addDefaultTraits(aquite, TinkerModifiers.overworked);
+            addDefaultTraits(charoite, TinkerModifiers.holy);
+            addDefaultTraits(diopside, TinkerModifiers.holy);
             addDefaultTraits(horizonite, TinkerModifiers.autosmelt, TinkerModifiers.fiery);
-            addDefaultTraits(pyrope, TinkerModifiers.overworked); // TODO: no default
+            addDefaultTraits(pyrope, TinkerModifiers.overworked);
         }
 
         @Nonnull
@@ -70,37 +69,43 @@ public class BlueMaterials extends AbstractMaterialDataProvider {
     }
 
     public static class BlueSkiesMaterialStats extends AbstractMaterialStatsDataProvider {
-
         public BlueSkiesMaterialStats(PackOutput packOutput, AbstractMaterialDataProvider materials) {
             super(packOutput, materials);
         }
 
         @Override
         protected void addMaterialStats() {
-            // TODO: these are all missing attackDamage
+            // melee, armor
             addMaterialStats(aquite,
-                    new HeadMaterialStats(300, 6f, DIAMOND, 2f),
-                    HandleMaterialStats.multipliers().durability(1.05f).miningSpeed(1.1f).attackSpeed(1.2f).build(),
+                    new HeadMaterialStats(270, 6f, Tiers.IRON, 2.25f),
+                    HandleMaterialStats.multipliers().durability(1.05f).miningSpeed(1.1f).attackSpeed(1.1f).attackDamage(0.9f).build(),
                     StatlessMaterialStats.BINDING);
 
+            // melee, armor
             addMaterialStats(charoite,
-                    new HeadMaterialStats(1500, 6.5f, DIAMOND, 2f),
-                    HandleMaterialStats.multipliers().durability(1.05f).miningSpeed(1.1f).attackSpeed(1.1f).build(),
+                    new HeadMaterialStats(1561, 8.0f, Tiers.DIAMOND, 2f),
+                    HandleMaterialStats.multipliers().durability(1.05f).miningSpeed(1.1f).attackSpeed(1.1f).attackDamage(1.05f).build(),
                     StatlessMaterialStats.BINDING);
 
+            // melee, armor
             addMaterialStats(diopside,
-                    new HeadMaterialStats(1600, 6f, DIAMOND, 6f),
-                    HandleMaterialStats.multipliers().durability(1.05f).miningSpeed(1.1f).attackSpeed(0.7f).build(),
+                    new HeadMaterialStats(1661, 6f, Tiers.DIAMOND, 3.25f),
+                    HandleMaterialStats.multipliers().durability(0.95f).miningSpeed(1.1f).attackSpeed(0.75f).attackDamage(1.25f).build(),
                     StatlessMaterialStats.BINDING);
 
+            // melee, armor
             addMaterialStats(horizonite,
-                    new HeadMaterialStats(300, 6.5f, DIAMOND, 3f),
-                    HandleMaterialStats.multipliers().durability(1.05f).miningSpeed(1.1f).attackSpeed(1f).build(),
+                    new HeadMaterialStats(250, 8.0f, Tiers.DIAMOND, 1.75f),
+                    HandleMaterialStats.multipliers().durability(1.05f).miningSpeed(1.1f).attackSpeed(1f).attackDamage(1.1f).build(),
                     StatlessMaterialStats.BINDING);
 
+            // TODO: for shields, plating
+//            addMaterialStats(moonstone, PlatingMaterialStats.SHIELD.getDefaultStats());
+
+            // melee, armor
             addMaterialStats(pyrope,
-                    new HeadMaterialStats(300, 5f, DIAMOND, 1f),
-                    HandleMaterialStats.multipliers().durability(1.05f).miningSpeed(1.1f).attackSpeed(1.1f).build(),
+                    new HeadMaterialStats(300, 11f, Tiers.STONE, 1.55f),
+                    HandleMaterialStats.multipliers().durability(1.05f).miningSpeed(1.1f).attackSpeed(1.25f).build(),
                     StatlessMaterialStats.BINDING);
         }
 
